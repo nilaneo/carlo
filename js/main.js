e8a840fde64eb9ace6618e3bf891cce2250d0999
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-02-11 17:48:45 +0200

    Added contacts page, small fixes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,15 @@
 $(document).ready(function(){
-	var screenHeight = $(window).height() - 162;
-	var productHeight = $('.container-product').height() + 100;
+	var screenHeight = $(window).height() - 162,
+		contentHeight = $(window).height() - 237,
+		productHeight = $('.container-product').height() + 100,
+		contactsHeight = $('.block-contacts').height() + 100;
 	$(".screen").height(Math.max(screenHeight, productHeight));
+
+	$(".screen-2").height(Math.max(contentHeight, contactsHeight));
 
 	// Closing popup
 
 	$(".btn-close-popup").on("click", function(){
 		$(this).closest(".popup-wrapper").hide();
-	});
-
-	// 	Changing quantity and price of order
-
-	// var $minus = $(".minus"),
-	// 	$plus = $(".plus");
-
-	// $minus.on("click", function() {
-	// 	var currentOrder = $(this).closest(".row-order"),
-	// 		quantityNumber = currentOrder.find(".quantity-number"),
-	// 		price = parseInt(currentTyre.find(".price-one").find("span").html()),
- //        	count = parseInt(quantityNumber.html()) - 1;
- //        count = count < 1 ? 1 : count;
- //        quantityNumber.html(count);
- //        quantityNumber.change();
- //        priceSet.find("span").html(parseInt(quantityNumber.html()) * priceOne);
- //        priceSet.find("span").change;
- //    });
-
- //    $plus.on("click", function() {
- //    	var currentTyre = $(this).parents(".chosen-tyre"),
-	// 		quantityNumber = currentTyre.find(".quantity-number"),
-	// 		priceOne = parseInt(currentTyre.find(".price-one").find("span").html()),
-	// 		priceSet = currentTyre.find(".price-set");
- //        quantityNumber.html(parseInt(quantityNumber.html()) + 1);
- //        quantityNumber.change();
- //        priceSet.find("span").html(parseInt(quantityNumber.html()) * priceOne);
- //        priceSet.find("span").change;
- //    });
+	});	
 });
